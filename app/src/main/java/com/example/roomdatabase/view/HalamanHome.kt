f81e67befd09d064acd5b19854abb41863828382
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2025-08-21 16:48:48 +0700

    HalamanHome.kt Progress

--- app/src/main/java/com/example/roomdatabase/view/HalamanHome.kt
+++ app/src/main/java/com/example/roomdatabase/view/HalamanHome.kt
@@ -1,3 +1,81 @@
 package com.example.roomdatabase.view
 
-object DestinasiHome :
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.R
+import com.example.roomdatabase.room.Siswa
+import com.example.roomdatabase.uicontroller.DestinasiNavigasi
+import com.example.roomdatabase.viewmodel.HomeViewModel
+import com.example.roomdatabase.viewmodel.PenyediaVIewModel
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: ()-> Unit,
+
+    modifier: Modifier = Modifier,
+
+    viewModel: HomeViewModel = ViewModel(factory = PenyediaVIewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                tittle = stringResource(DestinasiHome.titleRes),
+                canNaviagteBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        },
+    ){
+        innerPadding ->
+        val uiStateSiswa by viewModel.homeUiState.collectAsState()
+        BodyHome(
+            itemSiswa = uiStateSiswa.listSiswa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier){
+    Column {  }
+}
