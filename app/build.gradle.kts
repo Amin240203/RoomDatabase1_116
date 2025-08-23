4193f885def279ec222458e7ba0fba2a83f7c3fb
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2025-08-24 02:17:46 +0700

    Add and Complete PetaNavigasi.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.bundles.room)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     ksp(libs.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
