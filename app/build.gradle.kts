4a9956fbd8f13ccea1ccbd6caead46a8d3973218
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2025-08-24 09:20:22 +0700

    Bug, Exception And Error Fixing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2"
 }
 
 android {
